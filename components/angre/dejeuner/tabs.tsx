b4d0badc3e77e13b7400eb7bbc20a7488f87436d
nandju <nlbmd17@gmail.com>
2025-02-04 19:01:50 +0100

    avancement des images 2/10

--- components/angre/dejeuner/tabs.tsx
+++ components/angre/dejeuner/tabs.tsx
@@ -7,22 +7,28 @@ export default function FilterMenu() {
   return (
     <div className="flex flex-wrap justify-center pt-16 gap-2 md:gap-8">
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full">
-        <Tab key="dejeuners" title="Déjeuners" />
+        <Tab key="poulets-grilles" title="Poulets grillés" />
         </Tabs>
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
-        <Tab key="diners" title="Diners" />
+        <Tab key="plats" title="Les plats" />
         </Tabs>
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
-        <Tab key="frites" title="Frites" />
+        <Tab key="lunchs" title="Les Lunchs" />
         </Tabs>
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
-        <Tab key="menus-italiens" title="Menus italiens" />
+        <Tab key="poulet-braise" title="Poulet Braisé" />
         </Tabs>
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
-        <Tab key="menus-libanais" title="Menus libanais" />
+        <Tab key="burgers-sandwichs" title="Burgers & Swandichs" />
         </Tabs>
         <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
-        <Tab key="menus-chinois" title="Menus chinois" />
+        <Tab key="supplements" title="Suppléments" />
+        </Tabs>
+        <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
+        <Tab key="grandes-sauces" title="Grandes Sauces" />
+        </Tabs>
+        <Tabs color="primary" aria-label="Tabs variants" variant="light" radius="full" isDisabled>
+        <Tab key="boissons" title="Boissons" />
         </Tabs>
     </div>
   );
